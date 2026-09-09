e480e3f02649b7ce782009a59f0882a4c7c30d66
ea8-8 <ea88@rice.edu>
2026-09-09 14:49:33 -0500

    Project 1

---
+++ Project1_Fern.m
@@ -0,0 +1,35 @@
+% Ezinne Akabueze, CMOR 220, Spring 2026, Project 1: Grow a Fern
+% Project1_Fern.m
+% This coding program is tasked to create 2 different fern plots using a repetitive itirative
+% process that includes matrix multiplication
+% Last modified: 2/15/26
+
+clc;
+clear;
+
+disp('Project 1')
+
+% Defining all necessary functions for Project 1
+% Function for Part 1 that repeats plotting an updating point z,
+% eventually creating the simple fern, by analyzing the random number at 2
+% different conditions
+function PartOne;
+    
+end
+
+% Function for Part 2 that repeats plotting an updating point z,
+% eventually creating the advanced fern, by analyzing the random number at 4
+% different conditions
+function PartTwo;
+
+end
+
+% Part 1
+disp('Part 1, Simple Fern')
+% run the function PartOne with no inputs:
+PartOne
+
+% Part 2
+disp('Part 2, Advanced Fern')
+% run the function PartTwo with no inputs:
+PartTwo
